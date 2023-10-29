7b445c90a7fe9ed9f1459e4896cfec14a87803cd
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-10-29 14:40:50 +0700

    best seller tab

---
+++ src/pages/Home/TabProducts/BestSeller.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box } from '@mui/material';
+import RecipeReviewCard from './TabProducts';
+// call some components are defined
+import { MakeProductsCard, CustomizeButton } from '~/Layouts/DefaultLayout';
+import { products } from '~/Layouts/DefaultLayout';
+
+function BestSeller() {
+    const navigate = useNavigate();
+
+    const handleNavigateToShop = () => {
+        navigate('/shop');
+    };
+
+    
+    return (
+        <Box sx={{ mt: 4 }}>
+            <Box display="flex" sx={{ justifyContent: 'space-between' }}>
+                {products.map((product) => (
+                    <MakeProductsCard
+                        key={product.id}
+                        image={product.img}
+                        title={product.title}
+                        price={product.price}
+                        rating={product.rating}
+                        label={product.label}
+                        sx={{ margin: '0 20px' }}
+                    />
+                ))}
+            </Box>
+            <CustomizeButton variant="contained" onClick={handleNavigateToShop}>
+                More Items
+            </CustomizeButton>
+        </Box>
+    );
+}
+
+
+export default BestSeller;
